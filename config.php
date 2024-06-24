68fabd01e97a9ec2598a515e13f4133d46576b79
MamataCIngalagi <mamata.c.ingalagi.engi@gmail.com>
2024-06-24 12:55:45 +0530

    update2

--- config.php
+++ config.php
@@ -12,5 +12,6 @@ $connection = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
+    
 }
 ?>
